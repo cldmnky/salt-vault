461fcb787d515149b528a13e8f6295c4d276a294
Magnus Bengtsson <magnus.bengtsson@expressen.se>
2016-11-18 14:47:52 +0100

    Add listing of keys, according to #5

--- _modules/vault.py
+++ _modules/vault.py
@@ -90,3 +90,25 @@ def delete_secret(path):
   except Exception as e:
     log.error('Failed to write secret! {0}: {1}'.format(type(e).__name__, e))
     raise salt.exceptions.CommandExecutionError(e)
+
+def list_secrets(path):
+  '''
+  List secret keys at the path in vault. The vault policy used must allow this.
+  The path should end with a trailing slash.
+
+  CLI Example:
+
+  .. code-block:: bash
+      salt '*' vault.list_secrets "secret/my/"
+  '''
+  log.debug('Listing vault secret keys for {0} in {1}'
+            .format(__grains__['id'], path))
+  try:
+    url = 'v1/{0}'.format(path)
+    response = __utils__['vault.make_request']('LIST', url)
+    if response.status_code != 200:
+      response.raise_for_status()
+    return response.json()['data']
+  except Exception as e:
+    log.error('Failed to list secret keys! {0}: {1}'.format(type(e).__name__, e))
+    raise salt.exceptions.CommandExecutionError(e)
